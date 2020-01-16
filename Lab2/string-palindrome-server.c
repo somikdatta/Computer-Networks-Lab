0fb5d262eaed87f2e597aa66ef76c484e13bfcaf
Somik Datta <developersomik@gmail.com>
2020-01-16 08:29:17 +0530

    palindrome string check @ server

---
+++ Lab2/string-palindrome-server.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<arpa/inet.h>
+
+int main(){
+        int welcomeSocket,newSocket,port,flag=0;
+        printf("Enter port to listen on: ");
+        scanf("%d",&port);
+        char buffer[1024];
+        struct sockaddr_in serverAddr;
+        struct sockaddr_storage serverStorage;
+        socklen_t addr_size;
+        welcomeSocket = socket(PF_INET,SOCK_STREAM,0);
+        serverAddr.sin_family=AF_INET;
+        serverAddr.sin_port=htons(port);
+        serverAddr.sin_addr.s_addr= inet_addr("127.0.0.1");
+        bind(welcomeSocket,(struct sockaddr*)& serverAddr,sizeof(serverAddr));
+        if(listen(welcomeSocket,5)==0){
+          printf("Listening\n");
+        }
+        else{
+          printf("Error\n");
+        }
+        addr_size=sizeof(serverStorage);
+        newSocket = accept(welcomeSocket,(struct sockaddr*)&serverStorage,&addr_size);
+        recv(newSocket,buffer,1024,0);
+        printf("String received from client: %s\n",buffer);
+        int len=strlen(buffer);
+        for(int i=0;i<len/2;i++){
+          if(buffer[i]!=buffer[len-i-1]){
+            flag=1;
+            break;
+          }
+        }
+        send(newSocket,&flag,sizeof(int),0);
+        close(newSocket);
+        close(welcomeSocket);
+        return 0;
+      }
